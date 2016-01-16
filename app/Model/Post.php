ef42cbd3c4cb16019620b09078d14131e153260f
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-01-16 11:39:56 +0900

    bake model Post

---
+++ app/Model/Post.php
@@ -0,0 +1,36 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Post Model
+ *
+ */
+class Post extends AppModel {
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'title' => array(
+			'notBlank' => array(
+				'rule' => array('notBlank'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'body' => array(
+			'notBlank' => array(
+				'rule' => array('notBlank'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
