9f8db42ddecffae09a7713272ff59ca5c2224a77
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-01-16 13:27:53 +0900

    prepare crud functions

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -5,11 +5,78 @@ App::uses('AppController', 'Controller');
  */
 class PostsController extends AppController {
 
-/**
- * Scaffold
- *
- * @var mixed
- */
-	public $scaffold;
+    public $components = ['Paginator', 'Flash'];
+    public $helpers = ['Html', 'Form'];
 
+    public function index() {
+        $this->Paginator->settings = [
+            'limit' => 5,
+            'order' => ['Post.id' => 'desc']
+        ];
+        $this->set('posts', $this->Paginator->paginate());
+    }
+
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->Post->create();
+            if ($this->Post->save($this->request->data)) {
+                $this->Flash->success(__('Your post has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Flash->error(__('Unable to add your post.'));
+        }
+    }
+
+    public function view($id) {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        $post = $this->Post->findById($id);
+        if (!$post) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+        $this->set('post', $post);
+    }
+
+    public function edit($id = null) {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        $post = $this->Post->findById($id);
+        if (!$post) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        if ($this->request->is(array('post', 'put'))) {
+            $this->Post->id = $id;
+            if ($this->Post->save($this->request->data)) {
+                $this->Flash->success(__('Your post has been updated.'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Flash->error(__('Unable to update your post.'));
+        }
+
+        if (!$this->request->data) {
+            $this->request->data = $post;
+        }
+    }
+
+    public function delete($id) {
+        if ($this->request->is('get')) {
+            throw new MethodNotAllowedException();
+        }
+
+        if ($this->Post->delete($id)) {
+            $this->Flash->success(
+                __('The post with id: %s has been deleted.', h($id))
+            );
+        } else {
+            $this->Flash->error(
+                __('The post with id: %s could not be deleted.', h($id))
+            );
+        }
+        return $this->redirect(array('action' => 'index'));
+    }
 }
--- app/View/Layouts/default.ctp
+++ app/View/Layouts/default.ctp
@@ -31,9 +31,7 @@ $cakeDescription = __d('cake_dev', 'CakePHP: the rapid development php framework
 			<h1><?php echo $this->Html->link($cakeDescription, 'http://cakephp.org'); ?></h1>
 		</div>
 		<div id="content">
-
-			<?php echo $this->Session->flash(); ?>
-
+			<?php echo $this->Flash->render(); ?>
 			<?php echo $this->fetch('content'); ?>
 		</div>
 		<div id="footer">
