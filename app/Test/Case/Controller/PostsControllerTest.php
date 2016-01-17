d821c5425627700042b86211bf7df79a282fc1db
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-01-17 21:09:06 +0900

    add one post controller test

--- app/Test/Case/Controller/PostsControllerTest.php
+++ app/Test/Case/Controller/PostsControllerTest.php
@@ -6,13 +6,32 @@ App::uses('PostsController', 'Controller');
  */
 class PostsControllerTest extends ControllerTestCase {
 
-/**
- * Fixtures
- *
- * @var array
- */
-	public $fixtures = array(
-		'app.post'
-	);
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = array(
+        'app.post'
+    );
+
+    public function setUp() {
+        parent::setUp();
+        $this->controller = $this->generate('Posts', [
+            'components' => ['Paginator', 'Session'],
+            'models' => ['Post' => ['save']],
+            'methods' => ['redirect']
+        ]);
+        $this->controller->autoRender = false;
+    }
+
+
+    public function testIndexアクションではページングの結果がpostsにセットされること() {
+        $data = ['Post' => ['title' => 'sample', 'body' => 'blaaah']];
+        $this->controller->Paginator->expects($this->once())
+            ->method('paginate')->will($this->returnValue($data));
+        $vars = $this->testAction('/posts', ['method' => 'get', 'return' => 'vars']);
+        $this->assertEquals($data, $vars['posts']);
+    }
 
 }
